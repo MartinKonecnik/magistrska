4ca348582ea35d517226cbd81360c548b2f758ab
Martin Konečnik <martin.konecnik@cestel.si>
2025-08-13 19:19:20 +0200

    Change classifier to use Conv1d

--- classifier.py
+++ classifier.py
@@ -5,9 +5,9 @@ import torch
 class BinaryClassifier(torch.nn.Module):
     def __init__(self, input_dim):
         super(BinaryClassifier, self).__init__()
-        self.fc1 = torch.nn.Linear(input_dim, 64)
-        self.fc2 = torch.nn.Linear(64, 32)
-        self.fc3 = torch.nn.Linear(32, 1)
+        self.fc1 = torch.nn.Conv1d(input_dim, 64)
+        self.fc2 = torch.nn.Conv1d(64, 32)
+        self.fc3 = torch.nn.Conv1d(32, 1)
         self.dropout = torch.nn.Dropout(0.2)
 
     def forward(self, x):
